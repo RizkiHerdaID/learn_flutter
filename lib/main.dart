65dae161569cb810b9acad05b0e263e60291a170
Rizki Herdatullah <rizkiherda@gmail.com>
2021-02-18 23:25:29 +0700

    scaffold & appbar widgets

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,17 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text('hey rizki!'),
+    home: Scaffold(
+      appBar: AppBar(
+        title: Text('my first app'),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Text('hello rizki!'),
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Text('click'),
+      ),
+    )
   ));
 }
